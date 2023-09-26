32eadff485e1555ef6c3de47a64e0242e9389196
hayodavis <davis.fandi@gmail.com>
2023-09-26 21:49:55 +0700

    Penambahan fitur logout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -11,11 +12,34 @@ Future<void> main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: Routes.LOGIN,
-      getPages: AppPages.routes,
-    ),
-  );
+  runApp(StreamBuilder<User?>(
+    stream: FirebaseAuth.instance.authStateChanges(),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const MaterialApp(
+          home: Scaffold(
+              body: Center(
+            child: CircularProgressIndicator(),
+          )),
+        );
+      }
+
+      return GetMaterialApp(
+        title: "Application",
+        debugShowCheckedModeBanner: false,
+        initialRoute: snapshot.data != null ? AppPages.INITIAL : Routes.LOGIN,
+        getPages: AppPages.routes,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          fontFamily: 'inter',
+        ),
+      );
+    },
+  )
+      // GetMaterialApp(
+      //   title: "Application",
+      //   initialRoute: AppPages.INITIAL,
+      //   getPages: AppPages.routes,
+      // ),
+      );
 }
